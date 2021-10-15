e605c64cf31769a76eb04f06c414a2a69f1810c6
MATTEOTOSINI94 <matteotosini300@icloud.com>
2021-10-15 16:46:39 +0200

    mod

--- js/master.js
+++ js/master.js
@@ -3,7 +3,7 @@
 //  Si potrebbe fare saltando un if o comunque accorpando porzioni di codice.
 //  Occhio solo a non saltare troppi passaggi logici che per qualcuno sono ostici.
 
-// faccio inserire un numero all'utente
+
 const numberUser = Math.ceil(Math.random() * 100)
 
 if(numberUser % 2 ===0){
